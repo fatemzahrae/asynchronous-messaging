f755469885c8ea20b4961b2719e1d7041b412b52
tema <fatimazahrakribii012@gmail.com>
2025-03-12 00:22:54 +0000

    start over

--- src/main/java/org/com/lab1/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.com.lab1.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface ProductRepository extends JpaRepository<Product, UUID> {
-    Optional<Product> findByProductId(String productId);
-}
